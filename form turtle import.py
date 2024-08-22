6661a8b2337cd3018cbe50d3a1cc058d0b163bfb
Diegoperez <dperez-i033n@kinal.edu.gt>
2024-08-22 13:09:16 -0600

    Primer Commit

---
+++ form turtle import.py
@@ -0,0 +1,19 @@
+form turtle import
+import colorys
+
+
+bgcolor(' blackk ')
+tracer(2)
+pensize(2)
+h=0
+
+for  i in range(195);
+color=colorys.hsv_to_rgb(h,0.9,1)
+h+=0.006
+pencolor(color)
+It(179)
+backward(1*0.1)
+circle(i*0.3,120)
+rt(14)
+forward(i*0.1)
+circle(i*0,3120)
